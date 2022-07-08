4d2356e96e6c92743e03fa9602e081243e66a8d6
Otavio de Meira Lima <otaviomeira2008@hotmail.com>
2022-07-07 22:41:41 -0300

    New ipynb file for further analysis with models from scikit learn

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ from kd_tree import Kdtree, printPreorder
 from read_files import *
 from xnn import *
 
-databases = ['appendicitis', 'haberman', 'pima', 'led7digit', 'monk-2', 'heart', 'wdbc', 'phoneme', 'iris', 'ecoli', 'banana']
-databases=['ecoli']
+# databases = ['appendicitis', 'haberman', 'pima', 'led7digit', 'monk-2', 'heart', 'wdbc', 'phoneme', 'iris', 'ecoli', 'banana']
+databases = ["drug200"]
 
 k = 5
 print("Quantidades de vizinhos próximos calculados: ", k)
 
 for database in databases:
     print('Database: ' + database)
-    point_list = getDataPoints('data/' + database + '.dat')
+    point_list = getDataPoints('data/' + database + '.csv')
     trainingPoints, testPoints = getTrainingAndTestsPoints(point_list)
 
 
     xnn = Xnn(priority_queue=[])
     xnn.buildKdtree(trainingPoints)
     xnn.getStatisticsFromTestPoints(k, testPoints, getUniqueClasses(testPoints))
-    print(len(getUniqueClasses(testPoints)))
-    print("\n")
+    
